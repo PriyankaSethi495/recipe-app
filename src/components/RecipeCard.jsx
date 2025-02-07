b82e53a139189e2c21dd2beb67184710ecffbb7d
p559153 <priyanka@mail.upb.de>
2025-02-07 05:42:46 +0530

    added favorite button

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { addFavorite, removeFavorite } from '../redux/slices/favoritesSlice';
 import '../styles/RecipeCard.css';
 
 const RecipeCard = ({ meal }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites.items);
+  const isFavorite = favorites.find((item) => item.idMeal === meal.idMeal);
 
   const handleClick = () => {
     navigate(`/recipe/${meal.idMeal}`);
   };
 
+  const toggleFavorite = (e) => {
+    e.stopPropagation();
+    if (isFavorite) {
+      dispatch(removeFavorite(meal));
+    } else {
+      dispatch(addFavorite(meal));
+    }
+  };
+
   const instructions = meal.strInstructions 
-    ? meal.strInstructions.substring(0, 100) 
+    ? meal.strInstructions.substring(0, 100)
     : 'No instructions available. Click for details.';
 
   return (
@@ -18,6 +32,9 @@ const RecipeCard = ({ meal }) => {
       <img src={meal.strMealThumb} alt={meal.strMeal} />
       <h2>{meal.strMeal}</h2>
       <p>{instructions}...</p>
+      <button className="favorite-btn" onClick={toggleFavorite}>
+        {isFavorite ? 'Unfavorite' : 'Favorite'}
+      </button>
     </div>
   );
 };
